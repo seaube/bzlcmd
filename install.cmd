6d27ee421d2a53c3629aae7b279dbe57f73fd700
Ezekiel Warren <ezekiel@seaube.com>
2024-03-13 22:22:52 -0700

    chore: add simple install script for windows

---
+++ install.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+bazelisk build //bzlmod //bzlreg -c opt
+xcopy /Y /F "%~dp0bazel-bin\bzlmod\bzlmod.exe" "%USERPROFILE%\.local\bin\"
+xcopy /Y /F "%~dp0bazel-bin\bzlreg\bzlreg.exe" "%USERPROFILE%\.local\bin\"
