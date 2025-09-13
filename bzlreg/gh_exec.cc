647f4074d912cdbccb14571b5078e8d6c30a45b7
Ezekiel Warren <zaucy@users.noreply.github.com>
2025-09-12 18:34:15 -0700

    feat: infer version from git commit date (#109)

--- bzlreg/gh_exec.cc
+++ bzlreg/gh_exec.cc
@@ -105,3 +105,51 @@ auto bzlreg::gh_branch_commit_sha(
 
 	return line;
 }
+
+auto bzlreg::gh_commit_date(
+	std::string org,
+	std::string repo,
+	std::string commit
+) -> std::optional<std::string> {
+	auto gh = bp::search_path("gh");
+	if(gh.empty()) {
+		return {};
+	}
+
+	auto gh_proc_stdout = bp::ipstream{};
+	auto gh_proc_stderr = bp::ipstream{};
+	auto gh_proc = bp::child{
+		bp::exe(gh.generic_string()),
+		bp::args({
+			"api"s,
+			std::format("repos/{}/{}/git/commits/{}", org, repo, commit),
+			"--jq"s,
+			".author.date"s,
+		}),
+		bp::std_out > gh_proc_stdout,
+		bp::std_err > gh_proc_stderr,
+		bp::std_in < bp::null,
+	};
+
+	auto line = std::string{};
+	while(std::getline(gh_proc_stderr, line)) {
+		if(line.empty()) {
+			std::cerr << std::format("ERROR: {}\n", line);
+		}
+	}
+
+	std::getline(gh_proc_stdout, line);
+
+	gh_proc.wait();
+
+	auto proc_exit_code = gh_proc.exit_code();
+	if(proc_exit_code != 0) {
+		return {};
+	}
+
+	if(line.empty()) {
+		return {};
+	}
+
+	return line;
+}
